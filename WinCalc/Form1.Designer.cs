9017ae400b3351657bb8acc970fee5f569de75d9
Varankin Anatoliy <varankin@elewise.com>
2017-06-29 11:11:27 +0400

    fix wincalc

---
+++ WinCalc/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WinCalc
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lbOperations = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(383, 169);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(106, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Выйти";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDescription);
+            this.groupBox1.Controls.Add(this.lbOperations);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(501, 98);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Шаг 1 - Выбор операции";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblDescription.Enabled = false;
+            this.lblDescription.Location = new System.Drawing.Point(249, 16);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(249, 79);
+            this.lblDescription.TabIndex = 5;
+            this.lblDescription.Text = "Описание";
+            // 
+            // lbOperations
+            // 
+            this.lbOperations.ColumnWidth = 250;
+            this.lbOperations.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lbOperations.FormattingEnabled = true;
+            this.lbOperations.Location = new System.Drawing.Point(3, 16);
+            this.lbOperations.Name = "lbOperations";
+            this.lbOperations.Size = new System.Drawing.Size(246, 79);
+            this.lbOperations.TabIndex = 4;
+            this.lbOperations.SelectedIndexChanged += new System.EventHandler(this.lbOperations_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.lblResult);
+            this.groupBox2.Controls.Add(this.tbY);
+            this.groupBox2.Controls.Add(this.tbX);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(501, 58);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Шаг 2 - Ввод данных";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "=";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(237, 22);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(13, 13);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "?";
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(112, 19);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(100, 20);
+            this.tbY.TabIndex = 1;
+            this.tbY.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbY_KeyUp);
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(6, 19);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(100, 20);
+            this.tbX.TabIndex = 0;
+            this.tbX.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbX_KeyUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 200);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnClose);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(515, 200);
+            this.Name = "frmMain";
+            this.Text = "React Calculator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lbOperations;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WinCalc/Form1.cs
@@ -0,0 +1,149 @@
+﻿using ReactCalc;
+using ReactCalc.Models;
+//using ReactCalc.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinCalc
+{
+    public partial class frmMain : Form
+    {
+        private Calc Calc { get; set; }
+
+        private IOperation operation { get; set; }
+
+        private IOperation Operation {
+            get
+            {
+                return operation;
+            }
+            set
+            {
+                operation = value;
+                DispOperation = value as IDisplayOperation;
+            }
+        }
+
+        private IDisplayOperation DispOperation { get; set; }
+        
+        private DateTime? LastPressTime { get; set; }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Calc = new Calc();
+
+            var operations = Calc.Operations;
+
+            lbOperations.DataSource = operations;
+            lbOperations.DisplayMember = "Name";
+
+            lbOperations.SelectedIndex = 0;
+
+            lblResult.Text = "";
+
+            timer1.Interval = 300;
+        }
+
+        private void Calculate()
+        {
+            // определяем операцию
+            if (Operation == null)
+            {
+                lblResult.Text = "Выберите нормальную операцию";
+                return;
+            }
+
+            // определяем входные данные
+            var x = Calc.ToNumber(tbX.Text);
+            var y = Calc.ToNumber(tbY.Text);
+
+            try
+            {
+                // вычисляем
+                var result = Calc.Execute(Operation.Execute, new[] { x, y });
+                
+                var operName = DispOperation != null
+                    ? DispOperation.DisplayName
+                    : Operation.Name;
+
+                // возвращаем результат
+                lblResult.Text = $"{result}";
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = string.Format("Опаньки: {0}", ex.Message);
+            }
+        }
+
+        private void lbOperations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblDescription.Text = "";
+
+            Operation = lbOperations.SelectedItem as IOperation;
+
+            if (DispOperation != null)
+            {
+                lblDescription.Text = string.Format("Автор: {0}{1}Описание: {2}",
+                    DispOperation.Author,
+                    Environment.NewLine,
+                    !string.IsNullOrWhiteSpace(DispOperation.Description) ? DispOperation.Description : "нет"
+                    );
+            }
+            LastPressTime = DateTime.Now;
+            timer1.Start();
+        }
+
+        private void tbX_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Enter)
+            {
+                tbY.Focus();
+            }
+            else
+            {
+                LastPressTime = DateTime.Now;
+                timer1.Start();
+            }
+        }
+
+        private void tbY_KeyUp(object sender, KeyEventArgs e)
+        {
+            LastPressTime = DateTime.Now;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (LastPressTime.HasValue)
+            {
+                var diffTime = DateTime.Now - LastPressTime.Value;
+
+                if (diffTime.TotalMilliseconds >= 200)
+                {
+                    Calculate();
+                    LastPressTime = null;
+                    timer1.Stop();
+                }
+            }
+        }
+    }
+}
